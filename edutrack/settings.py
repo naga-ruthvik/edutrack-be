25a72fd6ed48fd6d34885834c10bbdf329901160
Naga Ruthvik <kondenagaruthvik@gmail.com>
2025-11-03 22:58:41 +0530

    "Added New serverless db setup"

--- edutrack/settings.py
+++ edutrack/settings.py
@@ -89,13 +89,23 @@ WSGI_APPLICATION = 'edutrack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),  # Or the IP/hostname of your PostgreSQL server
+        'PORT': '5432',       # Default PostgreSQL port
     }
-}
-
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
