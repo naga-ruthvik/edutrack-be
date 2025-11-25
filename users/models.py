66c5d93e7d5b001c6f006f2c5eccf693fc6b6af7
Naga Ruthvik <kondenagaruthvik@gmail.com>
2025-11-25 18:28:47 +0530

    FIx: Bug for giving error on null identifier of institution roles

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User, PermissionsMixin
+from django.db.models import Q
 
 class Profile(models.Model):
     """
@@ -28,10 +29,30 @@ class Profile(models.Model):
     )
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    identifier=models.CharField(max_length=50) # roll number - student, faculty_id - mentor a unique id which is given in a college
+    identifier=models.CharField(max_length=50, null=True, blank=True) # roll number - student, faculty_id - mentor a unique id which is given in a college
+    mentor = models.ForeignKey(
+        'self', 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True,
+        related_name='mentees', # Access students via: faculty_profile.mentees.all()
+        limit_choices_to={'role__in': ['FACULTY', 'HOD']}
+    )
 
     class Meta:
-        unique_together=('identifier', 'institution','department')
+        # Remove unique_together
+        # unique_together=('identifier', 'institution') 
+
+        # Add this instead:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['identifier', 'institution'],
+                name='unique_identifier_per_institution',
+                # 2. The Magic Condition:
+                # Only enforce uniqueness if 'identifier' is NOT null and NOT empty
+                condition=Q(identifier__isnull=False) & ~Q(identifier='')
+            )
+        ]
 
     def __str__(self):
         return self.user.email
